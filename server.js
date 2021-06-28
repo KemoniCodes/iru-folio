a200d9f5f8080cca48d339ad4a6d6d9393830368
KemoniCodes <kemoniw333@gmail.com>
2021-06-28 15:04:16 -0700

    build

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
